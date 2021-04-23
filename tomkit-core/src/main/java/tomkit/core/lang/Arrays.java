18ab9c504e2f6514f69471e65d2cff5efa087341
yanhu <yanhu32@163.com>
2021-04-23 18:28:31 +0800

    扩充数组工具类

--- tomkit-core/src/main/java/tomkit/core/lang/Arrays.java
+++ tomkit-core/src/main/java/tomkit/core/lang/Arrays.java
@@ -212,5 +212,576 @@ public final class Arrays {
         return new HashSet<>(java.util.Arrays.asList(a));
     }
 
+    /**
+     * 空数组
+     *
+     * @return 空数组
+     */
+    public static byte[] emptyBytes() {
+        return new byte[0];
+    }
+
+    /**
+     * 空数组
+     *
+     * @return 空数组
+     */
+    public static short[] emptyShorts() {
+        return new short[0];
+    }
+
+    /**
+     * 空数组
+     *
+     * @return 空数组
+     */
+    public static int[] emptyInts() {
+        return new int[0];
+    }
+
+    /**
+     * 空数组
+     *
+     * @return 空数组
+     */
+    public static long[] emptyLongs() {
+        return new long[0];
+    }
+
+    /**
+     * 空数组
+     *
+     * @return 空数组
+     */
+    public static float[] emptyFloats() {
+        return new float[0];
+    }
+
+    /**
+     * 空数组
+     *
+     * @return 空数组
+     */
+    public static double[] emptyDoubles() {
+        return new double[0];
+    }
+
+    /**
+     * 空数组
+     *
+     * @return 空数组
+     */
+    public static char[] emptyChars() {
+        return new char[0];
+    }
+
+    /**
+     * 空数组
+     *
+     * @return 空数组
+     */
+    public static boolean[] emptyBooleans() {
+        return new boolean[0];
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @param <T>          数组类型
+     * @return 当前数组或默认数组
+     */
+    public static <T> T[] defaultIfNull(T[] value, T[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @return 当前数组或默认数组
+     */
+    public static byte[] defaultIfNull(byte[] value, byte[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @return 当前数组或默认数组
+     */
+    public static short[] defaultIfNull(short[] value, short[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @return 当前数组或默认数组
+     */
+    public static int[] defaultIfNull(int[] value, int[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @return 当前数组或默认数组
+     */
+    public static long[] defaultIfNull(long[] value, long[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @return 当前数组或默认数组
+     */
+    public static float[] defaultIfNull(float[] value, float[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @return 当前数组或默认数组
+     */
+    public static double[] defaultIfNull(double[] value, double[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @return 当前数组或默认数组
+     */
+    public static char[] defaultIfNull(char[] value, char[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 返回当前数组，当前数组为{@code null}时返回默认数组
+     *
+     * @param value        当前数组
+     * @param defaultValue 默认数组
+     * @return 当前数组或默认数组
+     */
+    public static boolean[] defaultIfNull(boolean[] value, boolean[] defaultValue) {
+        return null != value ? value : defaultValue;
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @param <T>    数组类型
+     * @return 长度最小值
+     */
+    public static <T> int minLen(T[] array1, T[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最小值
+     */
+    public static int minLen(byte[] array1, byte[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最小值
+     */
+    public static int minLen(short[] array1, short[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最小值
+     */
+    public static int minLen(int[] array1, int[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最小值
+     */
+    public static int minLen(long[] array1, long[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最小值
+     */
+    public static int minLen(float[] array1, float[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最小值
+     */
+    public static int minLen(double[] array1, double[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最小值
+     */
+    public static int minLen(char[] array1, char[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最小值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最小值
+     */
+    public static int minLen(boolean[] array1, boolean[] array2) {
+        return Math.min(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @param <T>    数组类型
+     * @return 长度最大值
+     */
+    public static <T> int maxLen(T[] array1, T[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最大值
+     */
+    public static int maxLen(byte[] array1, byte[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最大值
+     */
+    public static int maxLen(short[] array1, short[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最大值
+     */
+    public static int maxLen(int[] array1, int[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最大值
+     */
+    public static int maxLen(long[] array1, long[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最大值
+     */
+    public static int maxLen(float[] array1, float[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最大值
+     */
+    public static int maxLen(double[] array1, double[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最大值
+     */
+    public static int maxLen(char[] array1, char[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组长度的最大值
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 长度最大值
+     */
+    public static int maxLen(boolean[] array1, boolean[] array2) {
+        return Math.max(array1.length, array2.length);
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @param <T>    数组类型
+     * @return 较小数组
+     */
+    public static <T> T[] min(T[] array1, T[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较小数组
+     */
+    public static byte[] min(byte[] array1, byte[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较小数组
+     */
+    public static short[] min(short[] array1, short[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较小数组
+     */
+    public static int[] min(int[] array1, int[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较小数组
+     */
+    public static long[] min(long[] array1, long[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较小数组
+     */
+    public static float[] min(float[] array1, float[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较小数组
+     */
+    public static double[] min(double[] array1, double[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较小数组
+     */
+    public static char[] min(char[] array1, char[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较小的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较小数组
+     */
+    public static boolean[] min(boolean[] array1, boolean[] array2) {
+        return array1.length > array2.length ? array2 : array1;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @param <T>    数组类型
+     * @return 较大数组
+     */
+    public static <T> T[] max(T[] array1, T[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较大数组
+     */
+    public static byte[] max(byte[] array1, byte[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较大数组
+     */
+    public static short[] max(short[] array1, short[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较大数组
+     */
+    public static int[] max(int[] array1, int[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较大数组
+     */
+    public static long[] max(long[] array1, long[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较大数组
+     */
+    public static float[] max(float[] array1, float[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较大数组
+     */
+    public static double[] max(double[] array1, double[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较大数组
+     */
+    public static char[] max(char[] array1, char[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
+
+    /**
+     * 获取两个数组中长度较大的，长度相对时返回第一个数组
+     *
+     * @param array1 数组1
+     * @param array2 数组2
+     * @return 较大数组
+     */
+    public static boolean[] max(boolean[] array1, boolean[] array2) {
+        return array1.length >= array2.length ? array1 : array2;
+    }
 
 }
